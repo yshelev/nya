a54286fd5511359228d527e98803926f6d03833b
zxcpisya <mamin.papa0@gmail.com>
2024-05-27 14:10:00 +1000

    окно открывается

--- WindowsScripts/main_window.py
+++ WindowsScripts/main_window.py
@@ -8,9 +8,26 @@ from PyQt5.QtWidgets import *
 
 from Workers.fileWorker import FileWorker
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.fileWorker = FileWorker()
-        uic.loadUi(self.fileWorker.get_file_from_WindowsUI("menu.ui"), self)
+        self.__fileWorker = FileWorker()
+        uic.loadUi(self.__fileWorker.get_file_from_WindowsUI("menu.ui"), self)
+
+        # setting on clicks. If you need set +1, just add (button, on_click)
+        self.set_on_click_on_several_buttons(
+            [
+                (self.employ_btn, self.on_employ_button_click),
+                (self.work_btn, self.on_work_button_click)
+            ],
+        )
+
+    def set_on_click_on_several_buttons(self, button_function: list[tuple]) -> None:
+        for button, function in button_function:
+            button.clicked.connect(function)
+
+    def on_employ_button_click(self):
+        print(1)
+
+    def on_work_button_click(self):
+        print(2)
