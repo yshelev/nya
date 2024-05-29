e72a97d38400bd44de2ff5c21954d5bb7631ac0e
zxcpisya <mamin.papa0@gmail.com>
2024-05-29 14:35:03 +1000

    окно открывается и закрывается

--- Workers/fileOpener.py
+++ Workers/fileOpener.py
@@ -1,4 +1,5 @@
 import json
+import sqlite3
 
 
 class FileOpener:
@@ -8,3 +9,11 @@ class FileOpener:
 	def jsonToDict(self, path_to_json_file: str) -> dict:
 		with open(path_to_json_file, "r", encoding="UTF-8") as filePointer:
 			return json.load(filePointer)
+
+	def sqliteToDict(self, path_to_db: str, query: str) -> list:
+		connection = sqlite3.connect(path_to_db)
+		cursor = connection.cursor()
+
+		query_result = cursor.execute(query).fetchall()
+
+		return query_result
